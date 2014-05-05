f73ec05ba05bd7691fdc735f76ba982b3592b971
悠仔 <rijian.lrj@taobao.com>
2014-05-05 11:44:19 +0800

    完成初始的配置和简单的代码

---
+++ ios/blogrsssdk.h
@@ -0,0 +1,13 @@
+//
+//  blogrsssdk.h
+//  blogrsssdk
+//
+//  Created by 罗 日健 on 5/4/14.
+//  Copyright (c) 2014 kenlist. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface blogrsssdk : NSObject
+
+@end
