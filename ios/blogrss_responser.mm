7968556535f6c4f1c8721b8a3bc48ffec488ab7b
悠仔 <rijian.lrj@taobao.com>
2014-05-14 09:10:17 +0800

    调整ios的代码

--- ios/blogrss_responser.mm
+++ ios/blogrss_responser.mm
@@ -13,18 +13,18 @@ BlogRSSResponser::~BlogRSSResponser() {
 
 }
   
-void BlogRSSResponser::OnRSSFetched(int ret_code, blogrss::RSSInfo rss_info) {
+void BlogRSSResponser::OnRSSFetched(int ret_code, blogrss::RSSItemList rss_item_list) {
   DCHECK(MessageLoop::current()->type() == MessageLoop::TYPE_UI);
   
   NSMutableArray* rss_items = [NSMutableArray array];
   
-  for (blogrss::RSSInfo::iterator iter = rss_info.begin(); iter != rss_info.end(); ++iter) {
+  for (blogrss::RSSItemList::iterator iter = rss_item_list.begin(); iter != rss_item_list.end(); ++iter) {
     RSSInfoItem* objc_rss_item = [[RSSInfoItem alloc] init];
-    blogrss::RSSItemInfo* cpp_rss_item = *iter;
+    blogrss::RSSItem* rss_item = *iter;
     
-    objc_rss_item.title = [NSString stringWithUTF8String:cpp_rss_item->title.c_str()];
-    objc_rss_item.pubDate = [NSDate dateWithTimeIntervalSince1970:mktime(&cpp_rss_item->pub_date)];
-    objc_rss_item.link = [NSString stringWithUTF8String:cpp_rss_item->link.c_str()];
+    objc_rss_item.title = [NSString stringWithUTF8String:rss_item->title.c_str()];
+    objc_rss_item.pubDate = [NSDate dateWithTimeIntervalSince1970:mktime(&rss_item->pub_date)];
+    objc_rss_item.link = [NSString stringWithUTF8String:rss_item->link.c_str()];
     
     [rss_items addObject:objc_rss_item];
   }
